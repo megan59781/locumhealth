8387605ab2155f5b68d4710a719f9eacf6cbe03c
unimegan59781 <up2061391@myport.ac.uk>
2024-04-08 13:06:47 +0100

    ability class and list display

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,13 +16,16 @@ class Login extends StatefulWidget {
   State createState() => LoginState();
 }
 
+/// The state class for the Login widget.
 class LoginState extends State<Login> {
   DatabaseReference dbhandler = FirebaseDatabase.instance.ref();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn();
 
-  
-
+  /// Retrieves the current latitude and longitude.
+  ///
+  /// Returns a Future that completes with a List containing the latitude and longitude.
+  /// If an error occurs, a default location [0.0, 0.0] is returned.
   Future<List<double>> getCurrentLatLong() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
@@ -35,12 +38,15 @@ class LoginState extends State<Login> {
 
       return [latitude, longitude];
     } catch (e) {
-      //print(e);
       // You might want to handle the error accordingly, for example, returning a default location.
       return [0.0, 0.0];
     }
   }
 
+  /// Adds a worker to the database if it doesn't already exist.
+  ///
+  /// Retrieves the current location and creates a worker object with the user's information.
+  /// The worker object is then pushed to the Firebase database under the "Worker" node.
   Future<void> addWorkerDb(user) async {
     dbhandler
         .child('Worker')
@@ -71,6 +77,10 @@ class LoginState extends State<Login> {
     });
   }
 
+  /// Adds a company to the database if it doesn't already exist.
+  ///
+  /// Creates a company object with the user's information and pushes it to the Firebase database
+  /// under the "Company" node.
   Future<void> addCompanyDb(user) async {
     dbhandler
         .child('Company')
@@ -95,7 +105,11 @@ class LoginState extends State<Login> {
     });
   }
 
-  // function to verfiy google account and return user info if valid
+  /// Verifies the Google account and returns the user information if valid.
+  ///
+  /// Uses the Google Sign-In package to authenticate the user with Google.
+  /// If the authentication is successful, the user information is returned.
+  /// If an error occurs, null is returned.
   Future<User?> _handleSignIn() async {
     try {
       GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
---
+++ lib/pages/worker/workerAbility.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:fyp/templates/displayText.dart';
+
+class WorkerAbility extends StatefulWidget {
+  final String workerId;
+
+  const WorkerAbility({Key? key, required this.workerId}) : super(key: key);
+
+  @override
+  State createState() => WorkerAbilityState();
+}
+
+class WorkerAbilityState extends State<WorkerAbility> {
+  List<String> selectedNames = [];
+  static const List<String> selections = <String>[
+    'First Aid',
+    'Manual Handling',
+    'Medicication Administration',
+    'Mental Health Training',
+    'Elderly Care Training',
+    'Child Care Training',
+    'Disability Care Training',
+    'Palliative Care Training',
+    'Dementia Care Training',
+    'Stoma Training',
+    'Catheter Training',
+    'PEG Feeding Training',
+    'Restrained Training',
+  ];
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 60),
+                const DisplayText(
+                    text: "Select Your Abilities",
+                    fontSize: 30,
+                    colour: Colors.black),
+                const SizedBox(height: 10),
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      final String ability = selections[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 6.0),
+                        child: Row(
+                          children: [
+                            Checkbox(
+                              value: selectedNames.contains(ability),
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  if (value != null && value) {
+                                    selectedNames.add(ability);
+                                  } else {
+                                    selectedNames.remove(ability);
+                                  }
+                                });
+                              },
+                            ),
+                            Text(
+                              ability,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                                decorationStyle: TextDecorationStyle.wavy,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: selections.length,
+                  ),
+                ),
+                const SizedBox(height: 30),
+              ]),
+        ),
+      ),
+    );
+  }
+}
